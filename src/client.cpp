189200c48b2323c68cd1bdc2dc2f25193664647f
Julian16124567 <julian.zotter.01@icloud.com>
2026-01-03 17:04:28 +0100

    finished client.cpp

--- src/client.cpp
+++ src/client.cpp
@@ -12,14 +12,20 @@ int main()
 
     boost::asio::io_context io;
     tcp::socket socket(io);
+    tcp::resolver resolver(io);
+    string message = "Connection Established";
 
     try
     {
+        auto endpoint = resolver.resolve(ip, port);
+        std::cout << "Connected to " << ip << ":" << port << std::endl;
+        boost::asio::write(socket, boost::asio::buffer(buffer));
     }
     catch (const std::exception &e)
     {
         std::cerr << e.what() << '\n';
     }
+    std::cout << "Connection not established or closed" << std::endl;
 
     return 0;
 }
